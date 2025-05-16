956a8b8035f6b979e231f4f81f56266ba0d44aa4
Kieran O'Neill <kieran@kibis.is>
2025-05-16 09:42:05 +0100

    feat(@kibisis/chains): add caip-002 namespace to base chain configurtion (#3)

--- packages/chains/src/types/chains/Chain.ts
+++ packages/chains/src/types/chains/Chain.ts
@@ -2,6 +2,7 @@
 import type { NativeCurrency, NodeCollection } from '@/types';
 
 /**
+ * @property {Record<'namespace', string>} caip002 - The CAIP-002 identifier for the family of chains.
  * @property {NodeCollection} algods - A collection of Algod nodes.
  * @property {string} displayName - A human-readable name for the chain.
  * @property {NodeCollection} indexers - [optional] A collection of Indexer nodes.
@@ -9,6 +10,7 @@ import type { NativeCurrency, NodeCollection } from '@/types';
  * @property {NativeCurrency} nativeCurrency - [optional] Details relating to the native currency of the chain.
  */
 interface Chain {
+  caip002: Record<'namespace', string>;
   algods: NodeCollection;
   displayName: string;
   indexers?: NodeCollection;
--- packages/chains/src/utilities/networkParametersFromChain.ts
+++ packages/chains/src/utilities/networkParametersFromChain.ts
@@ -1,12 +1,7 @@
-import axios, { type AxiosError } from 'axios';
+import axios from 'axios';
 
 // types
-import type {
-  ChainWithNetworkParameters,
-  GenesisResponse,
-  NetworkParametersFromChainParameters,
-  TransactionParamsResponse,
-} from '@/types';
+import type { Chain, ChainWithNetworkParameters, GenesisResponse, TransactionParamsResponse } from '@/types';
 
 // utilities
 import caip002FromGenesisHash from './caip002FromGenesisHash';
@@ -14,39 +9,28 @@ import defaultNode from './defaultNode';
 
 /**
  * Fetches network parameters for a given chain.
- * @param {NetworkParametersFromChainParameters} params - The chain information and a CAIP-002 namespace.
+ * @param {Chain} chain - The chain information.
  * @returns {Promise<ChainWithNetworkParameters>} A promise that resolves to the chain details enriched with network
  * parameters such as fee, genesis hash, and CAIP-002 identifier.
- * @throws {Error} Throws an error if unable to fetch network parameters from the supplied chain algod.
+ * @throws {AxiosError} If the default algod node in the supplied chain is unreachable.
  */
-export default async function networkParametersFromChain({
-  chain,
-  namespace,
-}: NetworkParametersFromChainParameters): Promise<ChainWithNetworkParameters> {
-  try {
-    const algod = defaultNode(chain.algods);
-    const [{ data: transactionParams }, { data: genesis }] = await Promise.all([
-      axios.get<TransactionParamsResponse>(`${algod.origin}/v2/transactions/params`),
-      axios.get<GenesisResponse>(`${algod.origin}/genesis`),
-    ]);
-    const genesisHash = transactionParams['genesis-hash'];
+export default async function networkParametersFromChain(chain: Chain): Promise<ChainWithNetworkParameters> {
+  const algod = defaultNode(chain.algods);
+  const [{ data: transactionParams }, { data: genesis }] = await Promise.all([
+    axios.get<TransactionParamsResponse>(`${algod.origin}/v2/transactions/params`),
+    axios.get<GenesisResponse>(`${algod.origin}/genesis`),
+  ]);
+  const genesisHash = transactionParams['genesis-hash'];
 
-    return {
-      ...chain,
-      caip002: caip002FromGenesisHash({
-        genesisHash,
-        namespace,
-      }),
-      fee: BigInt(transactionParams.fee),
-      feeSinkAddress: genesis.fees,
+  return {
+    ...chain,
+    caip002: caip002FromGenesisHash({
       genesisHash,
-      genesisID: transactionParams['genesis-id'],
-    };
-  } catch (error) {
-    if ((error as AxiosError).isAxiosError) {
-      throw new Error('unable to fetch network parameters');
-    }
-
-    throw error;
-  }
+      namespace: chain.caip002.namespace,
+    }),
+    fee: BigInt(transactionParams.fee),
+    feeSinkAddress: genesis.fees,
+    genesisHash,
+    genesisID: transactionParams['genesis-id'],
+  };
 }
