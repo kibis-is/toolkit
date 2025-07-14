42b6f73b929834ef65e3bd9f185b6e34173855ee
Kieran O'Neill <hello@kieranoneill.com>
2025-07-14 19:56:54 +0200

    fix(chains): exports algorand testnet

--- packages/chains/src/chains/index.ts
+++ packages/chains/src/chains/index.ts
@@ -1,5 +1,6 @@
 export { default as algorand } from './algorand';
 export { default as algorandBetanet } from './algorandBetanet';
+export { default as algorandTestnet } from './algorandTestnet';
 export { default as fnet } from './fnet';
 export { default as voi } from './voi';
 export { default as voiTestnet } from './voiTestnet';
