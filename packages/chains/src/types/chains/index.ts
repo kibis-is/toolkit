441c24c980de7f35f6a035805078f3a028eef10f
Kieran O'Neill <hello@kieranoneill.com>
2025-07-17 13:29:57 +0200

    chore: squash

--- packages/chains/src/types/chains/index.ts
+++ packages/chains/src/types/chains/index.ts
@@ -1,5 +1,5 @@
 export type { default as Chain } from './Chain';
-export type { default as ChainClass } from './ChainClass';
+export type { default as ChainConstructor } from './ChainConstructor';
 export type { default as ChainParameters } from './ChainParameters';
 export type { default as NetworkConfiguration } from './NetworkConfiguration';
 export type { default as NetworkInformation } from './NetworkInformation';
