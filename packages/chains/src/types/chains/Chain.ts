603dd27a47ebce40b93bda2e75eeae6a32a97f69
Kieran O'Neill <kieran@kibis.is>
2025-08-15 20:12:59 +0200

    fix(chains): uses conditional namespace for inferring chain type (#25)

--- packages/chains/src/types/chains/Chain.ts
+++ packages/chains/src/types/chains/Chain.ts
@@ -1,6 +1,15 @@
 // decorators
 import { AVMChain, EVMChain } from '@/decorators';
 
-type Chain = AVMChain | EVMChain;
+// enums
+import { CAIP002Namespace } from '@/enums';
+
+type Chain<Namespace = CAIP002Namespace> = Namespace extends CAIP002Namespace.Algorand
+  ? AVMChain
+  : Namespace extends CAIP002Namespace.AVM
+    ? AVMChain
+    : Namespace extends CAIP002Namespace.EIP155
+      ? EVMChain
+      : never;
 
 export default Chain;
